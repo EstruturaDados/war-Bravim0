6f58e976e6333e8f38af5bb5d302111ae04275a7
Bravim0 <thalisbravim123@gmail.com>
2025-10-22 20:35:57 +0000

    O comecço

--- jogotest.c
+++ jogotest.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+// Definição da struct Territorio
+typedef struct {
+    char nome[50];
+    char corExercito[20];
+    int tropas;
+} Territorio;
+
+int main() {
+    Territorio mapa[5]; // Vetor estático de 5 territórios
+
 
