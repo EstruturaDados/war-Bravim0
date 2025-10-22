8443bd053b4b3b5d509ed84ebe25b8c2d131bf61
Bravim0 <thalisbravim123@gmail.com>
2025-10-22 20:10:58 +0000

    Tentacidsbkfvnlnvl

--- test.c
+++ test.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+// definição da struch territorio
+typedef strucht {
+    char nome[50];
+    char corExercito[20];
+    int tropas;
+
+} Territorio;
+
+int maoin(){
+    Territorio mapa{5}; // Vetor estaticos dos territorios
+
+}
 
